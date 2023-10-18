db831819f82dea300640b7d9b83b4e21bb1a999b
selvasathis <127232661+selvasathis@users.noreply.github.com>
2023-10-18 22:59:49 +0530

    Update nexusSharedLibrary.groovy

--- vars/nexusSharedLibrary.groovy
+++ vars/nexusSharedLibrary.groovy
@@ -1,3 +1,7 @@
+    def scm_checkout() {
+        echo 'get the file'
+        git 'https://github.com/selvasathis/nexus.git'
+    }
     def npm_package() {
         echo 'running npm package'
         sh 'npm install'
