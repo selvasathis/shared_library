ae18cd5b3fcd2bf4d3df0ce492fe2d21c2d07a35
sathis <selvasathis28@gmail.com>
2023-11-01 22:46:17 +0530

    modify the file completely

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,8 +1,16 @@
-def call('mvn clean') {
-    sh 'mvn clean package'
+def mvnClean() {
+    sh 'mvn clean package',
     echo 'mvn clean package done'
 }
-def call('mvn test') {
-    sh 'mvn test'
+def mvnTest() {
+    sh 'mvn test',
     echo 'mvn test done'
 }
+// def call('mvn clean') {
+//     sh 'mvn clean package'
+//     echo 'mvn clean package done'
+// }
+// def call('mvn test') {
+//     sh 'mvn test'
+//     echo 'mvn test done'
+// }
